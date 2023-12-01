f6b3fddc0bbae2de5f9d9da4c5c6b0fa4f3e6bf6
Eros God <erosgod19921118@gmail.com>
2023-12-01 15:17:54 -0500

    GraphQL server on Ruby on Rails without JWT implemented by FMRGJ.

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Types
+  class QueryType < Types::BaseObject
+    field :all_links, [LinkType], null: false
+
+    def all_links
+      Link.all
+    end
+  end
+end
